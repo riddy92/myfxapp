c0d9ade03330cc3df940b7cdc473701336c1ebbf
Tola Huthma <ridwan.huthman@brevalusa.com>
2017-11-29 20:40:59 +0100

    css changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 	<div class="content-row">
 		<div class="col">
 			<div class="rates-panel-body">
-				<div class="rates-panel-heading">Market rates as at {{ date('d/m/Y')}}</div>
+				<div class="rates-panel-heading"> Naira market rates as at {{ date('d/m/Y')}}</div>
 
 				<div class="rates-panel-body">
 					<div class="currency-table">
@@ -38,13 +38,13 @@
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in est tortor. Aliquam non libero ut mi rhoncus faucibus eu in eros. Aliquam interdum vulputate libero in tristique. Donec venenatis feugiat nunc mattis tempor. Ut laoreet, purus ac volutpat fermentum, eros quam luctus justo, cursus consectetur orci lorem eu purus. 
                 </p>
             </div>
-        </div>
+    </div>
 
     {{--  Rates for the last 3 days  --}}
     <div class="content-row">
 		<div class="col">
 			<div class="rates-panel-body">
-				<div class="rates-panel-heading">Previous days rates</div>
+				<div class="rates-panel-heading">Naira Previous days rates</div>
 
                 <div class="rates-panel-body">
 					<div class="currency-table">
